6dc9d4c99ca9bec579943bb523988fb54f841850
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2015-12-24 11:32:08 +0200

    Correct pluralisation

--- minions/core/templatetags.py
+++ minions/core/templatetags.py
@@ -11,3 +11,18 @@ def updated_querystring(request, params):
             original_params.pop(key)
     original_params.update(params)
     return original_params.urlencode()
+
+
+@library.filter
+def ua_pluralize(value, bit1="", bit2="", bit3="", bit4=""):
+    try:
+        if value % 100 in (11, 12, 13, 14):
+            return bit3
+        if value % 10 == 1:
+            return bit1
+        if value % 10 in (2, 3, 4):
+            return bit2
+        else:
+            return bit3
+    except:
+        raise TemplateSyntaxError
