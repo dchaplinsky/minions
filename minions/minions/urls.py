227629335ca3fa3a7d2c9b8d558733aa3c2ea7b2
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2016-02-06 00:09:34 +0300

    Robots.txt, different tags for MP pages, meta keywords for homepage and convocation pages

--- minions/minions/urls.py
+++ minions/minions/urls.py
@@ -3,6 +3,7 @@ from django.contrib.sitemaps import GenericSitemap
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 from core.models import Convocation, MemberOfParliament, Minion
 
 urlpatterns = [
@@ -28,7 +29,10 @@ urlpatterns = [
                 'queryset': Minion.objects.all()
             }),
         }},
-        name='django.contrib.sitemaps.views.sitemap')
+        name='django.contrib.sitemaps.views.sitemap'),
+
+    url(r'^robots\.txt$', TemplateView.as_view(
+        template_name='robots.txt', content_type='text/plain')),
 
     # Нехай щастить!
     # url(r'^admin/', include(admin.site.urls)),
