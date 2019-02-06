3e91568dac4cb35b1dd20ebdb2d4752d489a400d
Stanyslav Yadykin <syadykin@gmail.com>
2019-02-06 15:39:53 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -xe
 VERSION=$(git rev-parse HEAD)
 TAG=$(git describe --tags 2>/dev/null || echo $VERSION)
 
-docker build -t minions:${TAG} -t minions:latest --build-arg version=${VERSION} .
+docker build -t posipaky.info:latest -t hub:5000/posipaky.info:latest --build-arg version=${VERSION} .
