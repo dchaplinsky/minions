8c69bca351a46e3e070afdcbf46b3b93c5cf225b
Stanyslav Yadykin <syadykin@gmail.com>
2019-02-06 17:15:13 +0200

    Correct image build tag

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 set -xe
 
 VERSION=$(git rev-parse HEAD)
-TAG=$(git describe --tags 2>/dev/null || echo $VERSION)
+TAG=$(git describe --tags 2>/dev/null || echo "latest")
 
-docker build -t posipaky.info:latest -t hub:5000/posipaky.info:latest --build-arg version=${VERSION} .
+docker build -t posipaky.info:${TAG} -t hub:5000/posipaky.info:${TAG} --build-arg version=${VERSION} .
